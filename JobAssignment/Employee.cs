6de23b1c043c2ce90b8c90e2918b8d3af4132081
TristanHillyard <73565670+TristanHillyard@users.noreply.github.com>
2021-01-01 12:52:47 -0700

    Add project files.

---
+++ JobAssignment/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JobAssignment
+{
+    public class Employee : Person
+    {
+        public int EmployeeId(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ JobAssignment/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace JobAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+            employee.SayName();
+        }
+    }
+}
